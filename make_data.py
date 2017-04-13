1be152eec052c20ee925ea42b502f2789c6b694a
MariLiis <mariliisvelner@gmail.com>
2017-04-13 19:00:43 +0300

    Refactored, removed unnecessary bits

--- make_data.py
+++ make_data.py
@@ -48,8 +48,6 @@ FEATURES = ["class", "electrode", "seg", "activity", "mobility", "complexity", "
 """
 Taken from the pyEEG module, added Hjorth activity (TP) to the returned values.
 """
-
-
 def hjorth(X, D=None):
     """ Compute Hjorth mobility and complexity of a time series from either two
     cases below:
@@ -106,85 +104,6 @@ def hjorth(X, D=None):
     return TP, np.sqrt(M2 / TP), np.sqrt(float(M4) * TP / M2 / M2)
     # Hjorth Activity, Mobility and Complexity. Changed: TP as Hjorth Activity is returned as well
 
-
-def bin_power(X, Band, Fs):
-    """Compute power in each frequency bin specified by Band from FFT result of
-    X. By default, X is a real signal.
-
-    Note
-    -----
-    A real signal can be synthesized, thus not real.
-
-    Parameters
-    -----------
-
-    Band
-        list
-
-        boundary frequencies (in Hz) of bins. They can be unequal bins, e.g.
-        [0.5,4,7,12,30] which are delta, theta, alpha and beta respectively.
-        You can also use range() function of Python to generate equal bins and
-        pass the generated list to this function.
-
-        Each element of Band is a physical frequency and shall not exceed the
-        Nyquist frequency, i.e., half of sampling frequency.
-
-     X
-        list
-
-        a 1-D real time series.
-
-    Fs
-        integer
-
-        the sampling rate in physical frequency
-
-    Returns
-    -------
-
-    Power
-        list
-
-        spectral power in each frequency bin.
-
-    Power_ratio
-        list
-
-        spectral power in each frequency bin normalized by total power in ALL
-        frequency bins.
-
-    """
-
-    C = np.fft.fft(X)
-    C = abs(C)
-    Power = np.zeros(len(Band) - 1)
-    for Freq_Index in range(0, len(Band) - 1):
-        Freq = float(Band[Freq_Index])
-        Next_Freq = float(Band[Freq_Index + 1])
-        Power[Freq_Index] = sum(
-            C[np.floor(
-                Freq / Fs * len(X)
-            ): np.floor(Next_Freq / Fs * len(X))]
-        )
-    Power_Ratio = Power / sum(Power)
-    return Power, Power_Ratio
-
-
-def bin_ps(window, bands, freq):
-    averaged = []
-    ps = np.abs(np.fft.fft(window)) ** 2
-    time_step = 1 / freq
-    freqs = list(np.fft.fftfreq(len(window), time_step))
-    idx = np.argsort(freqs)
-    ps = ps[idx]
-    for i in range(len(bands) - 1):
-        start = freqs.index(bands[i])
-        ps_band = ps[start: start + int((bands[i + 1] - bands[i]) / 0.1)]
-        ps_avg = sum(ps_band) / len(ps_band)
-        averaged.append(ps_avg)
-    return averaged
-
-
 def plot_and_show(x_axis, y_axis):
     plt.plot(x_axis, y_axis)
     plt.show()
@@ -203,13 +122,10 @@ def make_windows(sample, window_length, freq):
     time_windows = []
     for n in range(0, len(sample), data_points):
         new = sample[n:n + data_points]
-        # if len(new) == data_points:
-        #     new = sample[len(sample) - data_points:len(sample)]
         time_windows.append(new)
     return time_windows
 
 
-# extra_features = []
 """
 classif (int) -- 1 if the data is interictal files, 2 if preictal files
 mat_templ (str) -- the template for the mat-files
@@ -229,9 +145,8 @@ def get_state_data(classif, mat_templ, seg_templ, file_no, window_length):
         data = file[seg_templ.format(str(i))][0][0]
 
         # READ IN DATA
-        array = data["data"]  # numpy.ndarray, len(array[0]) = 239766, len(array) = 16
+        array = data["data"]
         sampling_freq = ceil(data["sampling_frequency"][0][0])
-        # print("sampling_freq: ", sampling_freq)
         # ROW_DURATION = data["data_length_sec"][0][0]
         # sequence_no = data["sequence"][0][0]
         # data["channels"] -- electrode names
@@ -249,21 +164,13 @@ def get_state_data(classif, mat_templ, seg_templ, file_no, window_length):
                 activity = hjorth_params[0]
                 mobility = hjorth_params[1]
                 complexity = hjorth_params[2]
-                hfd = pyeeg.hfd(window, 5)  # what is kmax??
+                hfd = pyeeg.hfd(window, 5)
                 skewness = st.skew(window)
                 kurtosis = st.kurtosis(window)
                 bins = pyeeg.bin_power(window, [0, 4, 8, 12, 30, 70, 180], sampling_freq)
 
-                # print(bin_ps(window, [0.5, 4, 7, 12, 30, 100], sampling_freq))
-                # ps = sig.welch(window, sampling_freq)
-                # if elc == 0 and i == 1:
-                #     global extra_features
-                #     extra_features = ["'" + str(i) for i in ps[0]]
-
                 datarow = [classif, elc + 1, i, activity, mobility, complexity, hfd, skewness, kurtosis] + list(
-                    bins[0]) + list(bins[1])  # + list(ps[1])
-                # print(len(datarow) == 8 + 5 + 5)  # 8 other parameters, power spectrum values were calculated for 5 bins
-                # first column 1 -- INTERICTAL; 2 -- PREICTAL
+                    bins[0]) + list(bins[1])
                 result.append(datarow)
     return result
 
@@ -318,29 +225,4 @@ dog_5 = get_object_data(DOG5_DIR, "Dog_5", DOG5_INTERICTALS, DOG5_PREICTALS, WIN
 # patient_2 = get_object_data(PATIENT2_DIR, "Patient_2", PATIENT2_INTERICTALS, PATIENT2_PREICTALS, WINDOW_LENGTH_1)
 
 dog_data = dog_5
-# patient_data = patient_2
-# FEATURES += extra_features
 write_to_file("dog5_minute_data.csv", FEATURES, dog_data)
-# write_to_file("patient2_10sec_data.csv", FEATURES, patient_data)
-
-# fft_windows.append(fft_row)
-
-"""
-For even N, the original order returned by FFT  is as follows:
-
-X[0] represents DC frequency component
-X[1] to X[N/2] terms are positive frequency components
-X[N/2+1] is the Nyquist frequency (F_s/2) that is common to both positive and negative frequencies. We will consider it as part of negative frequencies to have the same equivalence with the fftshift function.
-X[N/2+1] to X[N] terms are considered as negative frequency components
-
-Each point/bin in the FFT output array is spaced by the frequency resolution Δf, which is
-calculated as Δf = f_s / N,
-where f_s is the sampling frequency and N is the FFT size that is considered.
-Thus, in this case, each point in the array is spaced by the frequency resolution
-Δf = f_s / N = 400 / 4000 = 0.1 Hz
-
-For example, a 10 Hz cosine signal will leave a spike at the 10 / 10 = 1st sample, which
-is located at index 1.
-
-Therefore, from the frequency resolution, the entire frequency axis can be computed as:
-"""
