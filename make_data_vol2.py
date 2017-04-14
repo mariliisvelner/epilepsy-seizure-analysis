d9a93bd89cbd2eb205a665886c92a5864b553f06
MariLiis <mariliisvelner@gmail.com>
2017-04-14 23:40:13 +0300

    Removed many constants; are now given as commandline arguments + refactored and commented some

--- make_data_vol2.py
+++ make_data_vol2.py
@@ -2,28 +2,45 @@ import csv
 import pandas as pd
 import os
 
+import sys
 
-FEATURES = ["electrode", "activity", "mobility", "complexity", "hfd", "skewness", "kurtosis", "ps_delta",
-            "ps_theta", "ps_alpha", "ps_beta", "ps_lowgamma", "ps_highgamma", "psr_delta", "psr_theta", "psr_alpha",
-            "psr_beta", "psr_lowgamma", "psr_highgamma"]
-MAIN_DIR = "C:\\Users\\MariLiis\\Documents\\Ylikool\\THESIS\\Thesis"
-DATA_DIR = "C:\\Users\\MariLiis\\Documents\\Ylikool\\THESIS\\Thesis\\data"
+# The directory of the py files
+MAIN_DIR = sys.argv[1]
+# The directory of the csv files
+DATA_DIR = sys.argv[2]
+# The name of the file containing the old data
+OLD_FNM = sys.argv[3]
+# The file where the new data will be written
+NEW_FNM = sys.argv[4]
+# The file containing the features
+FEATURE_FILE = sys.argv[5]
 
-old_fnm = "dog5_10sec_data.csv"
-new_fnm = "dog5_10sec_data_vol2.csv"
+# Column names for electrode number, segment number and class (interictal (=1) or preictal (=2))
 ELEC_COL = "electrode"
 SEG_COL = "seg"
-STATE_COL = "class"
+CLASS_COL = "class"
+
+
+# Read in the features
+features_f = open(FEATURE_FILE, encoding="UTF-8")
+FEATURES = [line.strip() for line in features_f.readlines()]
+features_f.close()
+# These features cannot contain the class and segment column
+FEATURES.remove(CLASS_COL)
+FEATURES.remove(SEG_COL)
+
+assert ELEC_COL in FEATURES, "Column {} is not among the features!".format(ELEC_COL)
+assert SEG_COL in FEATURES, "Column {} is not among the features!".format(SEG_COL)
+assert CLASS_COL in FEATURES, "Column {} is not among the features!".format(CLASS_COL)
 
 """
-:param dir (str) -- the directory, where the data resides
 :param filename (str) -- the name of the file to read the data from
 
 :return the data (pandas DataFrame) from the given file
 """
-def read_data(dir, filename):
+def read_data(filename):
     print("Reading data...")
-    os.chdir(dir)
+    os.chdir(DATA_DIR)
     data = pd.read_csv(filename, delimiter=";")
     os.chdir(MAIN_DIR)
     return data
@@ -47,36 +64,68 @@ def write_to_file(file_name, headings, data):
         writer.writerow(row)
     result.close()
 
-def get_state_data(state, old_data, electrodes):
-    new_state_data = []
-    old_state_data = old_data[old_data[STATE_COL] == state]
-    segments = max(old_state_data[SEG_COL])
+
+"""
+:param class_ (int) -- 1 (interictal) or 2 (preictal)
+:param old_data (pandas DataFrame) -- the data which was previously calculated using make_data.py
+:param electrodes (int) -- the number of electrodes in the data
+
+Gets the data from old_data corresponding to class_, transforms it and returns the new data.
+In old_data, each sample represents a time window for a certain electrode. This means that there are "concurrent" 
+samples (sample for time window 1 and electrode 1; sample for time window 1 and electrode 2; ...; sample for time window 
+1 and electrode n; sample for time window 2 and electrode 1; ...).
+
+In the new data, each sample represents a time window -- the features of every electrode for this time window are 
+concatenated to a single row. This means that there are NO "concurrent" samples (sample for time window 1; sample for 
+time window 2; ...).
+"""
+def get_class_data(class_, old_data, electrodes):
+    new_class_data = []
+    # Get the <class_> data (interictal or preictal)
+    old_class_data = old_data[old_data[CLASS_COL] == class_]
+    segments = max(old_class_data[SEG_COL])
+
     for seg in range(segments):
-        seg_data = old_state_data[old_state_data[SEG_COL] == seg + 1]
+        # Gets the data of segment <seg + 1>
+        seg_data = old_class_data[old_class_data[SEG_COL] == seg + 1]
+
         electrode_data = []
+        # The data of segment <seg + 1> and electrode <elec> is in electrode_data[x-1]
         for elec in range(electrodes):
             electrode_data.append(seg_data[seg_data[ELEC_COL] == elec + 1])
 
+        # Checks if every list in electrode_data is of same length
+        # a.k.a has the same number of windows
         assert len(set([len(data) for data in electrode_data])) == 1, "Electrodes have different number of windows"
 
+        # The number of windows for each electrode in a segment
         windows = len(electrode_data[0])
+
+        # For every time window, concat the data that belongs to this time window
         for i in range(windows):
             new_line = []
             for elec in range(electrodes):
+                # electrode[elec] selects the data of the electrode <elec>
+                # .iloc[i] selects the data of the time window <i>
+                # [FEATURES] selects the data of every feature in FEATURES
                 new_line += electrode_data[elec].iloc[i][FEATURES].tolist()
-            new_state_data.append([state, seg + 1] + new_line)
-    return new_state_data
 
-old_data = read_data(DATA_DIR, old_fnm)
+            # At last, add the class and segment number to the features of the new data row
+            new_class_data.append([class_, seg + 1] + new_line)
+    return new_class_data
+
+old_data = read_data(OLD_FNM)
 electrodes = max(old_data[ELEC_COL])
-new_features = ["class", "seg"]
+new_features = [CLASS_COL, SEG_COL]
 
+# Add feature names for every electrode
 for j in range(electrodes):
     new_features += [feature + str(j + 1).zfill(2) for feature in FEATURES]
 
-interictal_data = get_state_data(1, old_data, electrodes)
-preical_data = get_state_data(2, old_data, electrodes)
+
+interictal_data = get_class_data(1, old_data, electrodes)
+preical_data = get_class_data(2, old_data, electrodes)
 
 new_data = interictal_data + preical_data
 
-write_to_file(new_fnm, new_features, new_data)
+write_to_file(NEW_FNM, new_features, new_data)
