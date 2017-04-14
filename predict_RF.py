dbdef674f00832b691ccfb6ace60f147d7c60e98
MariLiis <mariliisvelner@gmail.com>
2017-04-14 12:39:50 +0300

    Added feature importance calculation to the predicting_with_different_segs method; refactored some

--- predict_RF.py
+++ predict_RF.py
@@ -34,8 +34,6 @@ TO_PREDICT = "class"
 
 :return the data (pandas DataFrame) from the given file
 """
-
-
 def read_data(dir, filename):
     print("Reading data...")
     os.chdir(dir)
@@ -43,16 +41,13 @@ def read_data(dir, filename):
     os.chdir(MAIN_DIR)
     return data
 
-
 """
 :param filename (str) -- the name of the file where the data resides
-:param to_predict (str) -- the feature which is to be predicted
+:param to_predict (str) -- the name of the value which is to be predicted
 
 :return a tuple containing 1) the data (pandas DataFrame) used for training (and testing)
 2) the labels (pandas DataFrame) of the samples in the data 3) the features (list) of data  
 """
-
-
 def get_data_target_features(filename, to_predict):
     data = read_data(DATA_DIR, filename)
     features = [feature for feature in data if not feature in [TO_PREDICT, "seg"]]
@@ -68,8 +63,15 @@ def get_data_target_features(filename, to_predict):
     data_target = pd.concat([data_interictal, data_preictal], ignore_index=True)[to_predict]
     return data_features, data_target, features
 
+"""
+:param filename (str) -- the name of the file where the data resides
+:param to_predict (str) -- the name of the value which is to be predicted
+:param tries (int) -- number of times for the model to be fitted and test targets predicted 
 
-def cross_validate_different_segs(filename, to_predict, splits):
+Splits the data between train and test sets so that they contain different (but the same number of) segments,
+then fits the model and predicts <tries> times
+"""
+def predicting_with_different_segs(filename, to_predict, tries):
     data = read_data(DATA_DIR, filename)
     features = [feature for feature in data if not feature in [TO_PREDICT, "seg"]]
     print("Features: ")
@@ -88,8 +90,12 @@ def cross_validate_different_segs(filename, to_predict, splits):
     # The number of segments of a certain class (interictal and preictal) to include in the train data
     no_cl_seg = int(no_pr_seg / 2)
 
+    # Contains the accuracies for each prediction
     scores = []
-    for i in range(splits):
+    # Contains the sum of importances for every feature
+    feature_importances = [0 for feature in features]
+
+    for i in range(tries):
         # Shuffle the segment numbers
         preictal_segments = shuffle(preictal_segments)
         interictal_segments = shuffle(interictal_segments)
@@ -133,14 +139,36 @@ def cross_validate_different_segs(filename, to_predict, splits):
         print("Test data length: " + str(len(test_data)))
 
         # Fit and predict
-        rf = ensemble.RandomForestClassifier(n_estimators=10, max_features=None, class_weight="balanced")
-        rf.fit(train_data, train_target)
-        scores.append(rf.score(test_data, test_target))
+        rfc = ensemble.RandomForestClassifier(n_estimators=10, max_features=None, class_weight="balanced")
+        rfc.fit(train_data, train_target)
+        scores.append(rfc.score(test_data, test_target))
+
+        # Get the feature importances for this iteration and add the results to the sums in feature_importances
+        importances = rfc.feature_importances_
+        feature_importances = [importances[i] + feature_importances[i] for i in range(len(feature_importances))]
+
+    assert len(features) == len(feature_importances), "Length of features and feature importances do not match!"
+
+    # Calculate the average feature importance for every feature and print them out with rankings
+    feature_importances = [importance / tries for importance in feature_importances]
+    indices = np.argsort(feature_importances)[::-1]
+    print("Feature ranking (averaged): ")
+    for j in range(len(features)):
+        print("%d. feature %s (%f)" % (j + 1, features[indices[j]], feature_importances[indices[j]]))
 
     print("All scores: " + str(scores))
     print("Average score: " + str(sum(scores) / len(scores)))
 
+"""
+:param data (pandas DataFrame) -- the data to be used in predicting
+:param target (pandas DataFrame) -- the target values to be predicted
+:param splits (int) -- the number of splits to be made in cross validation
+:param is_shuffled (bool) -- True, if the data is to be shuffled before split into training and test data,
+otherwise False
 
+Performs cross-validation on the given data using StratifiedKFold and prints the scores along with the standard 
+deviation.
+"""
 def cross_validate(data, target, splits, is_shuffled):
     rf = ensemble.RandomForestClassifier(n_estimators=10, max_features=None, class_weight="balanced")
     skf = StratifiedKFold(n_splits=splits, shuffle=is_shuffled)
@@ -148,13 +176,16 @@ def cross_validate(data, target, splits, is_shuffled):
     scores = cross_val_score(rf, data, target, cv=skf)
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
+"""
+:param data (pandas DataFrame) -- the data used in predicting
+:param features (list) -- the features used in predicting
+:rfc (RandomForestClassifier) -- a previously fitted RandomForestClassifier model
 
-def get_feature_importances(data, target):
-    rf = ensemble.RandomForestClassifier(n_estimators=10, max_features=None, class_weight="balanced")
-    rf.fit(data, target)
-
-    importances = rf.feature_importances_
-    std = np.std([tree.feature_importances_ for tree in rf.estimators_], axis=0)
+Calculates the feature importances of the fitted model and plots them.
+"""
+def get_feature_importances(data, features, rfc):
+    importances = rfc.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in rfc.estimators_], axis=0)
     indices = np.argsort(importances)[::-1]
     print(data.shape)
     print("Feature ranking: ")
@@ -176,8 +207,10 @@ def get_feature_importances(data, target):
 # target = data_target_features[1]
 # features = data_target_features[2]
 
-# get_feature_importances(data, target)
+# rf = ensemble.RandomForestClassifier(n_estimators=10, max_features=None, class_weight="balanced")
+# rf.fit(data, target)
+# get_feature_importances(data, target, features, rf)
 #
 # cross_validate(data, target, 5, True)
 
-cross_validate_different_segs("patient1_minute_data_vol2.csv", TO_PREDICT, 5)
+predicting_with_different_segs("patient1_minute_data_vol2.csv", TO_PREDICT, 5)
