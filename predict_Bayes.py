e772fd2d4d77ae1ef2541d7b4633acfabd31fdda
MariLiis <mariliisvelner@gmail.com>
2017-04-16 17:06:50 +0300

    Added method for training and testing with different segments

--- predict_Bayes.py
+++ predict_Bayes.py
@@ -10,10 +10,15 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.utils import shuffle
+import numpy as np
 
 MAIN_DIR = sys.argv[1]
 DATA_DIR = sys.argv[2]
+# Name of the file
+FNM = sys.argv[3]
 TO_PREDICT = "class"
+
+print("File: " + FNM)
 
 """
 :param dir (str) -- the directory, where the data resides
@@ -61,7 +66,7 @@ def print_top_n_features(dict, n):
         print("{}. feature {} ({})".format(str(i + 1), dict[i][0], str(dict[i][1])))
 
 
-def fit_with_SelectKBest(data, target, k):
+def fit_with_SelectKBest(data, target, features, k):
     selector = SelectKBest(f_classif, k=k)
     data = selector.fit_transform(data, target)
 
@@ -82,6 +87,107 @@ def fit_with_SelectKBest(data, target, k):
     return data
 
 
+"""
+:param filename (str) -- the name of the file where the data resides
+:param to_predict (str) -- the name of the value which is to be predicted
+:param tries (int) -- number of times for the model to be fitted and test targets predicted 
+
+Splits the data between train and test sets so that they contain different (but the same number of) segments,
+then fits the model and predicts <tries> times
+"""
+def predicting_with_different_segs(filename, to_predict, tries, k):
+    data = read_data(filename)
+    features = [feature for feature in data if not feature in [TO_PREDICT, "seg"]]
+    print("Features: ")
+    print(features)
+
+    # Get preictal and interictal data
+    data_preictal = data[data[to_predict] == 2]
+    data_interictal = data[data[to_predict] == 1]
+
+    # Get the segment numbers of preictal and interictal segments
+    preictal_segments = list(set(data_preictal["seg"]))
+    interictal_segments = list(set(data_interictal["seg"]))
+
+    # Number of preictal segments
+    no_pr_seg = len(preictal_segments)
+    # The number of segments of a certain class (interictal and preictal) to include in the train data
+    no_cl_seg = int(no_pr_seg / 2)
+
+    # Contains the accuracies for each prediction
+    scores = []
+    # Contains the sum of importances for every feature
+    feature_rankings = [0 for feature in features]
+
+    for i in range(tries):
+        # Shuffle the segment numbers
+        preictal_segments = shuffle(preictal_segments)
+        interictal_segments = shuffle(interictal_segments)
+
+        # Get the TRAIN SEGMENTS as the FIRST no_cl_seg segments of the shuffled preictal and interictal segments
+        train_preictal_segments = preictal_segments[:no_cl_seg]
+        train_interictal_segments = interictal_segments[:no_cl_seg]
+        print("{}. Training set interictal segments: {}".format(str(i + 1), str(train_interictal_segments)))
+        print("{}. Training set preictal segments: {}".format(str(i + 1), str(train_preictal_segments)))
+
+        # Get the interictal windows, which's segments are in train_interictal_segments
+        train_interictal_windows = data_interictal[data_interictal["seg"].isin(train_interictal_segments)]
+        # Get the preictal windows, which's segments are in train_preictal_segments
+        train_preictal_windows = data_preictal[data_preictal["seg"].isin(train_preictal_segments)]
+        print("Training set no of interictal windows: " + str(len(train_interictal_windows)))
+        print("Training set no of preictal windows: " + str(len(train_preictal_windows)))
+
+        # Get the training set data and targets
+        train_data = pd.concat([train_interictal_windows, train_preictal_windows], ignore_index=True)[features]
+        train_target = pd.concat([train_interictal_windows, train_preictal_windows], ignore_index=True)[to_predict]
+
+        print("Training data length: " + str(len(train_data)))
+
+        # Get the TEST SEGMENTS as the LAST no_cl_seg segments of the shuffled preictal and interictal segments
+        test_preictal_segments = preictal_segments[no_cl_seg:]
+        test_interictal_segments = interictal_segments[no_cl_seg:no_cl_seg * 2]
+        print("{}. Test set interictal segments: {}".format(str(i + 1), str(test_interictal_segments)))
+        print("{}. Test set preictal segments: {}".format(str(i + 1), str(test_preictal_segments)))
+
+        # Get the interictal windows, which's segments are in test_interictal_segments
+        test_interictal_windows = data_interictal[data_interictal["seg"].isin(test_interictal_segments)]
+        # Get the interictal windows, which's segments are in test_preictal_segments
+        test_preictal_windows = data_preictal[data_preictal["seg"].isin(test_preictal_segments)]
+        print("Test set no of interictal windows: " + str(len(test_interictal_windows)))
+        print("Test set no of preictal windows: " + str(len(test_preictal_windows)))
+
+        # Get the test set data and targets
+        test_data = pd.concat([test_interictal_windows, test_preictal_windows], ignore_index=True)[features]
+        test_target = pd.concat([test_interictal_windows, test_preictal_windows], ignore_index=True)[to_predict]
+
+        print("Test data length: " + str(len(test_data)))
+
+        selector = SelectKBest(f_classif, k=k)
+        train_data = selector.fit_transform(train_data, train_target)
+
+        all_scores = selector.scores_
+        feature_rankings = [feature_rankings[i] + all_scores[i] for i in range(len(all_scores))]
+        new_features = [features[i] for i in range(len(features)) if selector.get_support()[i]]
+
+        print(type(test_data))
+        test_data = test_data[new_features]
+
+        # Fit and predict
+        gnb = GaussianNB()
+        gnb.fit(train_data, train_target)
+        scores.append(gnb.score(test_data, test_target))
+
+    # Calculate the average feature importance for every feature and print them out with rankings
+    feature_rankings = [ranking / tries for ranking in feature_rankings]
+    indices = np.argsort(feature_rankings)[::-1]
+    print("Feature ranking (averaged): ")
+    for j in range(len(features)):
+        print("%d. feature %s (%f)" % (j + 1, features[indices[j]], feature_rankings[indices[j]]))
+
+    print("All scores: " + str(scores))
+    print("Average score: " + str(sum(scores) / len(scores)))
+
+
 def cross_validate(data, target, splits, is_shuffled):
     clf = GaussianNB()
     skf = StratifiedKFold(n_splits=splits, shuffle=is_shuffled)
@@ -89,12 +195,14 @@ def cross_validate(data, target, splits, is_shuffled):
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
-data_target_features = get_data_target_features("patient1_10sec_data_vol2.csv", TO_PREDICT)
+# data_target_features = get_data_target_features(FNM, TO_PREDICT)
+#
+# data = data_target_features[0]
+# target = data_target_features[1]
+# features = data_target_features[2]
+#
+# data = fit_with_SelectKBest(data, target, 5)
+#
+# cross_validate(data, target, 5, True)
 
-data = data_target_features[0]
-target = data_target_features[1]
-features = data_target_features[2]
-
-data = fit_with_SelectKBest(data, target, 5)
-
-cross_validate(data, target, 5, True)
+predicting_with_different_segs(FNM, TO_PREDICT, 5, 20)
--- predict_LM.py
+++ predict_LM.py
@@ -1,6 +1,6 @@
 import os
 from collections import defaultdict
-
+import numpy as np
 import sys
 from sklearn import linear_model
 import pandas as pd
@@ -12,8 +12,11 @@ from sklearn.utils import shuffle
 
 MAIN_DIR = sys.argv[1]
 DATA_DIR = sys.argv[2]
+# Name of the file
+FNM = sys.argv[3]
 TO_PREDICT = "class"
 
+print("File: " + FNM)
 
 """
 :param filename (str) -- the name of the file to read the data from
@@ -98,13 +101,109 @@ def get_coef_values(data, target, features, iterations):
     for pair in coef_list:
         print(pair)
 
+"""
+:param filename (str) -- the name of the file where the data resides
+:param to_predict (str) -- the name of the value which is to be predicted
+:param tries (int) -- number of times for the model to be fitted and test targets predicted 
 
-data_target_features = get_data_target_features("patient1_10sec_data_vol2.csv", TO_PREDICT)
+Splits the data between train and test sets so that they contain different (but the same number of) segments,
+then fits the model and predicts <tries> times
+"""
+def predicting_with_different_segs(filename, to_predict, tries):
+    data = read_data(filename)
+    features = [feature for feature in data if not feature in [TO_PREDICT, "seg"]]
+    print("Features: ")
+    print(features)
 
-data = data_target_features[0]
-target = data_target_features[1]
-features = data_target_features[2]
+    # Get preictal and interictal data
+    data_preictal = data[data[to_predict] == 2]
+    data_interictal = data[data[to_predict] == 1]
 
-get_coef_values(data, target, features, 5)
+    # Get the segment numbers of preictal and interictal segments
+    preictal_segments = list(set(data_preictal["seg"]))
+    interictal_segments = list(set(data_interictal["seg"]))
 
-cross_validate(data, target, 5, True)
+    # Number of preictal segments
+    no_pr_seg = len(preictal_segments)
+    # The number of segments of a certain class (interictal and preictal) to include in the train data
+    no_cl_seg = int(no_pr_seg / 2)
+
+    # Contains the accuracies for each prediction
+    scores = []
+    # Contains the sum of importances for every feature
+    feature_coefficients = [0 for feature in features]
+
+    for i in range(tries):
+        # Shuffle the segment numbers
+        preictal_segments = shuffle(preictal_segments)
+        interictal_segments = shuffle(interictal_segments)
+
+        # Get the TRAIN SEGMENTS as the FIRST no_cl_seg segments of the shuffled preictal and interictal segments
+        train_preictal_segments = preictal_segments[:no_cl_seg]
+        train_interictal_segments = interictal_segments[:no_cl_seg]
+        print("{}. Training set interictal segments: {}".format(str(i + 1), str(train_interictal_segments)))
+        print("{}. Training set preictal segments: {}".format(str(i + 1), str(train_preictal_segments)))
+
+        # Get the interictal windows, which's segments are in train_interictal_segments
+        train_interictal_windows = data_interictal[data_interictal["seg"].isin(train_interictal_segments)]
+        # Get the preictal windows, which's segments are in train_preictal_segments
+        train_preictal_windows = data_preictal[data_preictal["seg"].isin(train_preictal_segments)]
+        print("Training set no of interictal windows: " + str(len(train_interictal_windows)))
+        print("Training set no of preictal windows: " + str(len(train_preictal_windows)))
+
+        # Get the training set data and targets
+        train_data = pd.concat([train_interictal_windows, train_preictal_windows], ignore_index=True)[features]
+        train_target = pd.concat([train_interictal_windows, train_preictal_windows], ignore_index=True)[to_predict]
+
+        print("Training data length: " + str(len(train_data)))
+
+        # Get the TEST SEGMENTS as the LAST no_cl_seg segments of the shuffled preictal and interictal segments
+        test_preictal_segments = preictal_segments[no_cl_seg:]
+        test_interictal_segments = interictal_segments[no_cl_seg:no_cl_seg * 2]
+        print("{}. Test set interictal segments: {}".format(str(i + 1), str(test_interictal_segments)))
+        print("{}. Test set preictal segments: {}".format(str(i + 1), str(test_preictal_segments)))
+
+        # Get the interictal windows, which's segments are in test_interictal_segments
+        test_interictal_windows = data_interictal[data_interictal["seg"].isin(test_interictal_segments)]
+        # Get the interictal windows, which's segments are in test_preictal_segments
+        test_preictal_windows = data_preictal[data_preictal["seg"].isin(test_preictal_segments)]
+        print("Test set no of interictal windows: " + str(len(test_interictal_windows)))
+        print("Test set no of preictal windows: " + str(len(test_preictal_windows)))
+
+        # Get the test set data and targets
+        test_data = pd.concat([test_interictal_windows, test_preictal_windows], ignore_index=True)[features]
+        test_target = pd.concat([test_interictal_windows, test_preictal_windows], ignore_index=True)[to_predict]
+
+        print("Test data length: " + str(len(test_data)))
+
+        # Fit and predict
+        lm = linear_model.LogisticRegressionCV(solver="liblinear", penalty="l1")
+        lm.fit(train_data, train_target)
+        scores.append(lm.score(test_data, test_target))
+
+        coef = lm.coef_[0]
+        feature_coefficients = [abs(coef[i]) + feature_coefficients[i] for i in range(len(feature_coefficients))]
+
+    assert len(features) == len(feature_coefficients), "Length of features and feature coefficients do not match!"
+
+    # Calculate the average feature importance for every feature and print them out with rankings
+    feature_coefficients = [coef / tries for coef in feature_coefficients]
+    indices = np.argsort(feature_coefficients)[::-1]
+    print("Feature coef. ranking (averaged): ")
+    for j in range(len(features)):
+        print("%d. feature %s (%f)" % (j + 1, features[indices[j]], feature_coefficients[indices[j]]))
+
+    print("All scores: " + str(scores))
+    print("Average score: " + str(sum(scores) / len(scores)))
+
+# data_target_features = get_data_target_features("patient1_minute_data_vol2.csv", TO_PREDICT)
+#
+# data = data_target_features[0]
+# target = data_target_features[1]
+# features = data_target_features[2]
+#
+# get_coef_values(data, target, features, 5)
+#
+# cross_validate(data, target, 5, True)
+
+predicting_with_different_segs(FNM, TO_PREDICT, 5)
